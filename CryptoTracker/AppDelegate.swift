07c2dbbdbbde3d510127097c6c78877451d4c74e
XenBits <106202889+XenBits@users.noreply.github.com>
2020-05-22 10:13:45 +0000

    add ref

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -11,4 +11,5 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     var menuBarCoinViewModel: MenuBarCoinViewModel!
-    var popoverCoinViewModel: PopoverCoinView
+    var popoverCoinViewModel: PopoverCoinViewModel!
+    
