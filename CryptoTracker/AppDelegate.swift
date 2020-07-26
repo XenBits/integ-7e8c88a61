beb1d928816ef7d3ff1eff0bac219bec5197a4f5
XenBits <106202889+XenBits@users.noreply.github.com>
2020-07-26 22:54:56 +0000

    fix compile error.

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -47,4 +47,4 @@ extension AppDelegate {
               let menuButton = statusItem.button
         else {return}
         
-        let hostingView = NSHostingView(rootView: MenuBarCoinView(viewModel: menu
+        let hostingView = NSHostingView(rootView: MenuBarCoinView(viewModel: menuBarCoinV
