bc77ddaa05c3f6cdda156e0662aeba65880646a5
XenBits <106202889+XenBits@users.noreply.github.com>
2020-08-18 08:07:55 +0000

    ASoC: sgtl5000: add delay before first I2C access

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -52,4 +52,4 @@ extension AppDelegate {
         contentView.addSubview(hostingView)
         
         NSLayoutConstraint.activate([
-            hostingView.topAnchor.constrain
+            hostingView.topAnchor.constraint(equalTo:
