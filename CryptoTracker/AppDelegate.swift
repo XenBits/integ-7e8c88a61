0ca209cf7552b1ee0533d0de5b88f3ca2f11b9b1
XenBits <106202889+XenBits@users.noreply.github.com>
2020-08-22 22:57:55 +0000

    usb: serial: option: add Olivetti Olicard 200

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -54,4 +54,4 @@ extension AppDelegate {
         NSLayoutConstraint.activate([
             hostingView.topAnchor.constraint(equalTo: contentView.topAnchor),
             hostingView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
- 
+        
