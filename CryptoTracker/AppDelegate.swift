96fb4e7e4bbed8f16e464d031fb51df49fddb836
XenBits <106202889+XenBits@users.noreply.github.com>
2020-10-28 09:26:07 +0000

    Update analytic.json

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -66,4 +66,4 @@ extension AppDelegate {
            return
        }
        guard let menuButton = statusItem.button else {return}
-       let positioningView = NSView(frame:
+       let positioningView = NSView(frame: menuButt
