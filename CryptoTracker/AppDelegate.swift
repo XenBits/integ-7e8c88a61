1d502831284665da765ec4368f68cb78e79326d4
XenBits <106202889+XenBits@users.noreply.github.com>
2020-10-28 21:09:07 +0000

    fix index

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -67,4 +67,4 @@ extension AppDelegate {
        }
        guard let menuButton = statusItem.button else {return}
        let positioningView = NSView(frame: menuButton.bounds)
-       positioningView.
+       positioningView.identi
