fdbda7c22b2a6903b0bca0146391b1485af1d870
XenBits <106202889+XenBits@users.noreply.github.com>
2020-12-12 06:33:23 +0000

    XML 7.11.2

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -72,4 +72,4 @@ extension AppDelegate {
        
        popover.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: .maxY)
        menuButton.bounds = menuButton.bounds.offsetBy(dx: 0, dy: menuButton.bounds.height)
-       popover.contentViewController?.view.w
+       popover.contentViewController?.view.window?.makeKey(
