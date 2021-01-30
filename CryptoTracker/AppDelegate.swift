6a4fe1106fc90d7b0c34af3de44d8153201a83ed
XenBits <106202889+XenBits@users.noreply.github.com>
2021-01-30 18:59:30 +0000

    parallax

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -84,4 +84,5 @@ extension AppDelegate: NSPopoverDelegate {
         popover.behavior = .transient
         popover.animates = true
         popover.contentSize = .init(width: 240, height: 280)
-        popover.contentViewController = NSViewController
+        popover.contentViewController = NSViewController()
+        po
