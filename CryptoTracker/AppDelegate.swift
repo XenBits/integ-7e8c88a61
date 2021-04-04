11a7256179cb8792a0e98e688bdfcede1b91d443
XenBits <106202889+XenBits@users.noreply.github.com>
2021-04-04 10:56:41 +0000

    Add context

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -92,4 +92,4 @@ extension AppDelegate: NSPopoverDelegate {
     }
     
     func popoverDidClose(_ notification: Notification) {
-        let positioningView = statusItem.button?.s
+        let positioningView = statusItem.button?.subviews.fi
