2cd5e4ccb85762977b34d31d65807b8c3b238150
XenBits <106202889+XenBits@users.noreply.github.com>
2021-07-31 21:46:24 +0000

    Upgraded gem(s): sass

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -13,4 +13,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
     
     private let session = URLSession(configuration: .default)
     private var wsTask: URLSessionWebSocketTask?
-    private 
+    private var pingTryCo
