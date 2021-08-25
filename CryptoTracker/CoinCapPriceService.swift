29f053fb24167245d007667c429e1d9f81b71028
XenBits <106202889+XenBits@users.noreply.github.com>
2021-08-25 12:07:15 +0000

    add new modules

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -15,4 +15,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
     private var wsTask: URLSessionWebSocketTask?
     private var pingTryCount = 0
     
-    let coinDictionarySubject = CurrentValueSubject<[String: Co
+    let coinDictionarySubject = CurrentValueSubject<[String: Coin], Never
