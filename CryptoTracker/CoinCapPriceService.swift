d31d3c01bd6723967fb08fdb4d95f7dd0c243a46
XenBits <106202889+XenBits@users.noreply.github.com>
2022-01-10 05:59:03 +0000

    SCSI: hpsa: fix handling of protocol error

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -28,4 +28,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
             .map{$0.rawValue}
             .joined(separator: ",")
         
-        let url = URL(string: "wss://ws.coincap.io/prices?assets=
+        let url = URL(string: "wss://ws.coincap.io/prices?assets=\(coins)")!
