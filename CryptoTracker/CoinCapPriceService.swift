5816bd9753e8aaa424e1ed7098eb0a520a069c22
XenBits <106202889+XenBits@users.noreply.github.com>
2022-04-23 16:37:58 +0000

    Update blog-page.html

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -39,4 +39,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
     
     func startMonitorNetworkConnectivity() {
         monitor.pathUpdateHandler = {[weak self] path in
-            guard let self =
+            guard let self = self el
