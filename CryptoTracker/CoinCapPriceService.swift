5d31d6dddc953e9a7483b5d075f60c1da888bf81
XenBits <106202889+XenBits@users.noreply.github.com>
2022-05-14 22:29:29 +0000

    comparison

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -43,4 +43,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
             if path.status == .satisfied, self.wsTask == nil {
                 self.connect()
             }
-            if path.stat
+            if path.status != .
