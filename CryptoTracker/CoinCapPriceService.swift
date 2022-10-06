10d33961c4a3d63c553cf6ef439c16470cba5a5c
XenBits <106202889+XenBits@users.noreply.github.com>
2022-10-06 21:05:12 +0000

    new tutorial

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -79,4 +79,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
     }
     
     private func onReceiveData(_ data: Data) {
-        guard let dictionary = try? JSONSerialization.json
+        guard let dictionary = try? JSONSerialization.jsonObject(with
