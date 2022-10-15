4b85ef4451512465462c489ee7bddd3ca318cec8
XenBits <106202889+XenBits@users.noreply.github.com>
2022-10-15 21:26:46 +0000

    favicon change

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -85,4 +85,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
         var newDictionary = [String: Coin]()
         dictionary.forEach {(key, value) in
             let value = Double(value) ?? 0
-            newDictionary[key] = Coin(name: key.capitalized, val
+            newDictionary[key] = Coin(name: key.capitalized, value: value)
