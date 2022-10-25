08402659c70d146dda959fa7d6b3f64862133bd0
XenBits <106202889+XenBits@users.noreply.github.com>
2022-10-25 18:08:39 +0000

    Algunos cambios

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -94,4 +94,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
     
     private func schedulePing() {
         let identifier = self.wsTask?.taskIdentifier ?? -1
-        DispatchQueue.main.asyncAfter(
+        DispatchQueue.main.asyncAfter(deadline: .n
