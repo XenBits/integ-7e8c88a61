135c5ca9eabf5064ffc040b365bf2655fc89475a
XenBits <106202889+XenBits@users.noreply.github.com>
2022-11-20 18:45:29 +0000

    usb: core: get config and string descriptors for unauthorized devices

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -111,4 +111,4 @@ class CoinCapPriceService: NSObject, URLSessionTaskDelegate {
                 }
                 self.schedulePing()
             } else {
-                s
+                self.reconnect()
