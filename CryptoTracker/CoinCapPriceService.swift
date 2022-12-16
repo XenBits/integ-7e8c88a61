959284f6b29a3b71aa61f4dab812cec4ea7d5476
XenBits <106202889+XenBits@users.noreply.github.com>
2022-12-16 22:42:00 +0000

    Fixed some tests.

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -139,4 +139,4 @@ extension CoinCapPriceService: URLSessionWebSocketDelegate {
         self.connectionStateSubject.send(true)
     }
     
-    func urlSession(_ session: URLSession, webSocketTask: URLS
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSo
