0a9f066719a84c90fb63fcd7f4822c8dac8bf910
XenBits <106202889+XenBits@users.noreply.github.com>
2022-12-17 20:22:31 +0000

    Bump eslint from 7.9.0 to 7.10.0

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -139,4 +139,4 @@ extension CoinCapPriceService: URLSessionWebSocketDelegate {
         self.connectionStateSubject.send(true)
     }
     
-    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, did
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith 
