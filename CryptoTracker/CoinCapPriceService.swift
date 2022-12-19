f67943015a1ed5849c65a23a2012977fcb2c6457
XenBits <106202889+XenBits@users.noreply.github.com>
2022-12-19 22:15:06 +0000

    Delete resume.pdf

--- CryptoTracker/CoinCapPriceService.swift
+++ CryptoTracker/CoinCapPriceService.swift
@@ -139,4 +139,4 @@ extension CoinCapPriceService: URLSessionWebSocketDelegate {
         self.connectionStateSubject.send(true)
     }
     
-    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCod
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, rea
