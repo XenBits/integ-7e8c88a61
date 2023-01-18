17aa7eaae79c3d3428badc4d6f4cb1d696a72518
XenBits <106202889+XenBits@users.noreply.github.com>
2023-01-18 15:43:56 +0000

    Finalizado

--- CryptoTracker/MenuBarCoinViewModel.swift
+++ CryptoTracker/MenuBarCoinViewModel.swift
@@ -3,3 +3,20 @@
 //  MenuBarCoinViewModel.swift
 //  CryptoTracker
 //
+//  Created by David on 2/9/22.
+//
+
+import Combine
+import Foundation
+import SwiftUI
+
+class MenuBarCoinViewModel: ObservableObject {
+    @Published private(set) var name: String
+    @Published private(set) var value: String
+    @Published private(set) var color: Color
+    @AppStorage("SelectedCoinType") private(set) var selecterdCoinType = CoinType.bitcoin
+    
+    private let service: CoinCapPriceService
+    private var subscrition = Set<AnyCancellable>()
+    
+    private let currencyFormatter: NumberFormatter = {
