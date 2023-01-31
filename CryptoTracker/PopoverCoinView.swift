95a96d4c82ad3410f7c325332f090776a0f18104
XenBits <106202889+XenBits@users.noreply.github.com>
2023-01-31 08:45:47 +0000

    \tnew file:   main.go

--- CryptoTracker/PopoverCoinView.swift
+++ CryptoTracker/PopoverCoinView.swift
@@ -2,3 +2,14 @@
 //
 //  PopoverCoinView.swift
 //  CryptoTracker
+//
+//  Created by David on 2/9/22.
+//
+
+import SwiftUI
+
+struct PopoverCoinView: View {
+    @ObservedObject var viewModel: PopoverCoinViewModel
+    
+    var body: some View {
+        VStack(spacing: 16){
