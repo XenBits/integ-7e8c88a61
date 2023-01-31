0882d2e24a2644fb1b6731df44e12cb142efb694
XenBits <106202889+XenBits@users.noreply.github.com>
2023-01-31 15:12:11 +0000

    travis-ci config

--- CryptoTracker/PopoverCoinView.swift
+++ CryptoTracker/PopoverCoinView.swift
@@ -13,3 +13,20 @@ struct PopoverCoinView: View {
     
     var body: some View {
         VStack(spacing: 16){
+            VStack {
+                Text(viewModel.title).font(.largeTitle)
+                Text(viewModel.subtitle).font(.title.bold())
+            }
+            Divider()
+            Picker("Select Coin", selection: $viewModel.selecterdCoinType){
+                ForEach(viewModel.coinType){ type in
+                    HStack {
+                        Text(type.desccription).font(.headline)
+                        Spacer()
+                        Text(viewModel.valueText(for: type))
+                            .frame(alignment: .trailing)
+                            .font(.body)
+                        
+                        Link(destination: type.url){
+                            Image(systemName: "safari")
+                        }
