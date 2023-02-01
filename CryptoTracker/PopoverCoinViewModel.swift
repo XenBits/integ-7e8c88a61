f7c710dee04d3229c2a1dfb2b9e741d603934e86
XenBits <106202889+XenBits@users.noreply.github.com>
2023-02-01 15:30:53 +0000

    minor whitespace fixes

--- CryptoTracker/PopoverCoinViewModel.swift
+++ CryptoTracker/PopoverCoinViewModel.swift
@@ -2,3 +2,15 @@
 //
 //  PopoverCoinViewModel.swift
 //  CryptoTracker
+//
+//  Created by David on 2/9/22.
+//
+
+import Combine
+import Foundation
+import SwiftUI
+
+class PopoverCoinViewModel: ObservableObject {
+    @Published private(set) var title: String
+    @Published private(set) var subtitle: String
+    @Published private(set) var coinType: [CoinType]
